e9f2a3dbf0bd26c3c337c1de15e093f2ad96692a
jeonghan-yun <jeonghan.yun@meshkorea.net>
2025-01-27 15:52:49 +0900

    first commit

---
+++ src/main/java/com/example/goodcodeeasyread/GoodCodeEasyReadApplication.java
@@ -0,0 +1,13 @@
+package com.example.goodcodeeasyread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodCodeEasyReadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodCodeEasyReadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/goodcodeeasyread/GoodCodeEasyReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goodcodeeasyread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodCodeEasyReadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
